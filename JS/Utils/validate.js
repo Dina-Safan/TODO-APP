459ba5dd9d04373fe7f34eceb80eeaacedb89c07
dinasafan <dinasafan83@gmail.com>
2025-11-06 03:09:10 +0200

    add task and validate

--- JS/Utils/validate.js
+++ JS/Utils/validate.js
@@ -1,10 +1,20 @@
 export const nameRegex= /^[A-Z][a-z]{2,}$/;
 export const emailRegex=/^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/;
 export const passwordRegex = /^\S{6,}$/;
+export const titleRegex = /^[A-Za-z\s]{3,}$/;
+export const descriptionRegex = /^.{5,}$/;
+
 
 export function validation(element,regex){
     if(regex.test(element.value)){
-       element.nextElementSibling.classList.add("invisible");
+        if(regex== titleRegex || regex==descriptionRegex){
+             element.nextElementSibling.classList.add("d-none");
+        }
+        else{
+            element.nextElementSibling.classList.add("invisible");
+
+        }
+       
        element.classList.add("is-valid");
        element.classList.remove("is-invalid");  
          
@@ -12,7 +22,13 @@ export function validation(element,regex){
     }
 
     else{
-         element.nextElementSibling.classList.remove("invisible");
+           if(regex== titleRegex || regex==descriptionRegex){
+             element.nextElementSibling.classList.remove("d-none");
+        }
+        else{
+          element.nextElementSibling.classList.remove("invisible");
+        }
+        
          element.classList.remove("is-valid");
          element.classList.add("is-invalid");  
        
--- JS/index.js
+++ JS/index.js
@@ -1,21 +1,56 @@
+import { descriptionRegex, titleRegex, validation } from "./Utils/validate.js";
+import { showToast } from './Utils/ui.js';
+
 //* Html Element
 const btnAddElement=document.querySelector("main .btn-add  button");
 const modalElement=document.querySelector(".modal");
+const statusInput =document.getElementById("statusInput");
+const categoryInput =document.getElementById("categoryInput");
+const titleInput =document.getElementById("titleInput");
+const descriptionInput =document.getElementById("descriptionInput");
+const addTaskBtn =document.getElementById("addTaskBtn");
+const updateTaskBtn =document.getElementById("updateTaskBtn");
 
 
 
 //^ Variables
-
+let taskArr=getTasks();
 
 //~ Function
-//show modal fun
+//show modal Function
 function showModal(){
     modalElement.classList.replace("d-none" ,"d-block");
 }
 
-//hide modal function
+//hide modal Function
 function hideModal(){
       modalElement.classList.replace("d-block" ,"d-none");
+}
+
+//set tasks in local storage Function
+function setTasks(){
+localStorage.setItem(`tasks_${currentUser.Email}`,JSON.stringify(taskArr));
+}
+
+//get tasks from local storage function
+function getTasks(){
+    return JSON.parse(localStorage.getItem(`tasks_${currentUser.Email}`))||[];
+}
+
+//display Function
+function addTask(){
+   if(validation(titleInput,titleRegex) && validation(descriptionInput,descriptionRegex)){
+     const task={
+        status:statusInput.value,
+        category:categoryInput.value,
+        title:titleInput.value,
+        description:descriptionInput.value,
+    }
+    taskArr.push(task);
+    setTasks();
+    hideModal();
+   }
+
 }
 
 //& Events
@@ -23,11 +58,19 @@ function hideModal(){
 //show modal
 btnAddElement.addEventListener("click",showModal)
 
-//hide modal
+//hide modal (First Way)
 modalElement.addEventListener("click",function(e){
-    if(e.target == modalElement.querySelector(".container"))hideModal()
+    if(e.target === modalElement || e.target==modalElement.querySelector(".container"))hideModal() 
 })
-
+          //(Second Way)
 window.addEventListener("keyup",function(e){
     if(e.code=="Escape") hideModal()
 })
+
+//add task
+addTaskBtn.addEventListener("click",addTask)
+
+//validate titile 
+titleInput.addEventListener("input",function(e){validation(titleInput,titleRegex)});
+//validate description
+descriptionInput.addEventListener("input",function(e){validation(descriptionInput,descriptionRegex)});
